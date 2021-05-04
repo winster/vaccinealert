544102221b689386d181c30b34bdaa2307941c72
Winster Jose - w0j015x <winster.jose@walmart.com>
2021-05-05 02:22:13 +0530

    attempt to resolve 403 thrown by cloud platform(s)

--- src/main/java/org/win/cowin/vaccinealert/service/SubscriptionService.java
+++ src/main/java/org/win/cowin/vaccinealert/service/SubscriptionService.java
@@ -175,9 +175,8 @@ public class SubscriptionService {
     log.info("inside initDistricts");
     Map<Integer, Boolean> districtMap = new HashMap<>();
     HttpHeaders headers = new HttpHeaders();
-    headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-    headers.add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
-    HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+    headers.set("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
+    HttpEntity entity = new HttpEntity(headers);
     ResponseEntity<States> stateResponseEntity = restTemplate.exchange(stateUrl, HttpMethod.GET, entity, States.class);
 
     //States states = restTemplate.getForObject(stateUrl, States.class);
